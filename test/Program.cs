6dcded8c091aa4b2752752331a2665925176d57c
Beloglazov Alexey <barber004@mail.ru>
2022-08-13 00:03:51 +0300

    testproverka

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(" number (a) :");
+float a = float.Parse(Console.ReadLine());
+Console.WriteLine(" number (b) :");
+float b =  float.Parse(Console.ReadLine());
+
+
+float res;
+res = a * b;
+Console.WriteLine("Общая сумма");
+Console.WriteLine(res);
+
+
+
+
+
+
+
+
+
