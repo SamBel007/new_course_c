1fe3378686177ac6dd9c726c746aecb4c0aaf2c9
Beloglazov Alexey <barber004@mail.ru>
2022-08-12 09:14:42 +0300

    6test1

---
+++ 6hw1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Write quantity of numbers: ");
+int num = int.Parse(Console.ReadLine());
+int a = 0;
+int Number = 0;
+for(int i=0;i<num;i++)
+{
+    Console.WriteLine("Write number: ");
+    a = int.Parse(Console.ReadLine());
+    if(a>0)
+        Number++;
+}
+Console.WriteLine($"{Number} numbers are positive");
